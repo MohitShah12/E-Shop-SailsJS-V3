e32c6f0e3bb2b9b1431d43d62fa86f700d44cdf5
MohitShah12 <159542156+MohitShah12@users.noreply.github.com>
2024-02-28 12:53:53 +0530

    Update bootstrap.js

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -36,7 +36,7 @@ module.exports.bootstrap = async function() {
     const hashPassword = await bcrypt.hash(process.env.ADMIN_PASS,12)
     console.log('hashpassword',hashPassword)
     await User.create({
-      email:'abc@abc.com',
+      email:process.env.ADMIN_EMAIL,
       password:hashPassword,
       name:'Admin',
       mobileno:'1234567890',
